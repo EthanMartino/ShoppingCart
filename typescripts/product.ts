82c9f6b1834d3fe530bed6831588d83939df5990
Ethan Martino <49508204+ET7026@users.noreply.github.com>
2019-11-12 13:46:45 -0800

    Created a product class

---
+++ typescripts/product.ts
@@ -0,0 +1,28 @@
+/**
+ * Represents a single shopping cart item
+ */
+class Product{
+    /**
+     * The Name of the product
+     */
+    title: string;
+
+    /**
+     * The Description of the product
+     */
+    description: string;
+
+    /**
+     * The price of the product
+     */
+    price: Number;
+
+}
+
+//Test Code
+/*
+let prod = new Product();
+prod.title = "Thing";
+prod.description = "Some shit";
+prod.price = 12332.43;
+*/
